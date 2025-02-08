f117a6c60254b2f155a5a4a6854a8d1a5e4a673a
zash0203 <zash0203@gmail.com>
2025-02-08 15:49:56 -0500

    Updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,11 +10,9 @@ const Navbar = () => {
       <ul className='flex justify-center gap-4 items-center'>
         <Link href="/"><li>Home</li></Link>
         <Link href="/about"><li>About</li></Link>
-        {/* <Link href="/shorten"><li>Shorten</li></Link> */}
-        {/* <Link href="/contact"><li>Contact Us</li></Link> */}
         <li className='flex gap-3'>
           <Link href="/shorten"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
-          <Link href="/github"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>GitHub</button></Link>
+          <Link href="https://github.com/zash0203/Shortify"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>GitHub</button></Link>
         </li>
       </ul>
     </nav>
