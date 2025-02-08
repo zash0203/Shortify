0f9e0a9bceb4fbfce86a95a4c673e947bdea7e56
zash0203 <zash0203@gmail.com>
2025-02-08 15:38:05 -0500

    Added About Me

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='p-6 bg-gray-100 rounded-lg shadow-lg'>
+            <p className='text-lg text-gray-800 leading-relaxed font-medium'>
+                Hello! My name is <span className='font-semibold text-blue-600'>Zeeshan</span>, currently working as a <span className='font-semibold text-green-600'>QA Analyst Co-op</span> at the <span className='font-semibold text-red-600'>Ministry of Education</span>. I am a passionate computer science student studying at <span className='font-semibold text-purple-600'>Toronto Metropolitan University</span>. Passionate about technology, thrives on collaborative projects, constantly seeks new challenges, and stays updated with the latest industry trends. I value work-life balance and enjoy outdoor activities like hiking and swimming.
+            </p>
+        </div>
+    );
+};
+
+export default About;
+
